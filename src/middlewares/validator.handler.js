5c3f2999207ac920213be47d6013cf0f42c6140a
Arnaldo Borda <aborda001@gmail.com>
2022-06-12 04:27:05 -0400

    Deleting boom errors handler

--- src/middlewares/validator.handler.js
+++ src/middlewares/validator.handler.js
@@ -1,12 +1,12 @@
-const boom = require('@hapi/boom');
-
 const validatorHandler = (schema, property) => {
   return (req, res, next) => {
     const data = req[property];
     const { error } = schema.validate(data, { abortEarly: false });
-    if (error) {
-      next(boom.badRequest(error));
-    }
+    if (error)
+      return res.status(400).json({
+        error: 'Validation failed',
+        message: error.details.map((e) => e.message).join(', '),
+      });
     next();
   };
 };
