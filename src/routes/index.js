383883d020a233c4d6214b2a638f2adfee2db252
Arnaldo Borda <aborda001@gmail.com>
2022-06-12 04:27:57 -0400

    Auth routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/users', require('./users.routes'));
+  router.use('/auth', require('./auth.routes'));
 }
 
 module.exports = routerApi;
