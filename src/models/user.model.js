01d32655dc75fdd6f05195385a4dc18e565edc6f
Arnaldo Borda <aborda001@gmail.com>
2022-06-12 00:59:00 -0400

    User db model

---
+++ src/models/user.model.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+userSchema.statics.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+userSchema.statics.comparePassword = async (password, hash) => {
+  return await bcrypt.compare(password, hash);
+};
+
+module.exports = model('User', userSchema);
