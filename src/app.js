41d636069131363ee393efcd1b748cdb13c3af61
Arnaldo Borda <aborda001@gmail.com>
2022-06-12 00:58:36 -0400

    Using errors middlewares

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
 
@@ -8,5 +13,9 @@ app.use(express.json());
 app.use(cors());
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 module.exports = app;
