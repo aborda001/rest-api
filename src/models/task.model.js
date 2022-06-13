7ed6d181421d54cec6ff5219b33ed6e2812490ea
Arnaldo Borda <aborda001@gmail.com>
2022-06-13 01:25:39 -0400

    Adding tasks list

---
+++ src/models/task.model.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const taskSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    done: {
+      type: Boolean,
+      default: false,
+    },
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+module.exports = model('Task', taskSchema);
