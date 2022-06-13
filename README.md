558a3011ade8a661928bc888532b41f019f34ea9
Arnaldo Borda <aborda001@gmail.com>
2022-06-13 11:06:21 -0400

    README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Getting started
+
+To get the Node server running locally:
+
+- Clone this repo
+- `npm install` to install all required dependencies
+- `npm run start:dev` to start the local server with hot reloading
+- `npm run start` to start the local server without hot reloading
+
+Testing
+
+- `npm run test` to run the tests
+- `npm run test:watch` to run the tests in watch mode
+
+# Code Overview
+
+## Dependencies
+
+- [expressjs](https://github.com/expressjs/express) - The server for handling and routing HTTP requests
+- [jsonwebtoken](https://github.com/auth0/node-jsonwebtoken) - For generating JWTs used by authentication
+- [mongoose](https://github.com/Automattic/mongoose) - For modeling and mapping MongoDB data to javascript objects
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,10 +2,12 @@ const supertest = require('supertest');
 const app = require('../src/app');
 const server = require('../src/index');
 const User = require('../src/models/user.model');
+const Task = require('../src/models/task.model');
 const api = supertest(app);
 
 beforeAll(async () => {
   await User.deleteMany({});
+  await Task.deleteMany({});
 });
 
 const users = [
@@ -17,6 +19,10 @@ const users = [
     username: 'agooduser001',
     password: 'test021220',
   },
+  {
+    username: 'validuser',
+    password: 'test031220',
+  },
 ];
 
 describe('Users methods', () => {
@@ -24,6 +30,14 @@ describe('Users methods', () => {
     await api
       .post('/api/v1/users')
       .send(users[1])
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('should respond with a 201 status code', async () => {
+    await api
+      .post('/api/v1/users')
+      .send(users[2])
       .expect(201)
       .expect('Content-Type', /application\/json/);
   });
@@ -59,6 +73,19 @@ describe('Users methods', () => {
       .expect('Content-Type', /application\/json/)
       .expect(/agooduser002/);
   });
+
+  test('should respond with a 200 status code', async () => {
+    const response = await api.post('/api/v1/auth').send(users[2]);
+
+    await api
+      .delete('/api/v1/users/')
+      .set({
+        'x-access-token': response.body.token,
+      })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect(/deleted/);
+  });
 });
 
 describe('Auth methods ', () => {
@@ -82,6 +109,138 @@ describe('Auth methods ', () => {
       .send(users[0])
       .expect(401)
       .expect('Content-Type', /application\/json/);
+  });
+
+  test('should responde with a 401 status code', async () => {
+    await api
+      .post('/api/v1/auth')
+      .send({
+        username: 'bad',
+        password: 'test011220',
+      })
+      .expect(401)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('should responde with a 401 status code', async () => {
+    await api
+      .get('/api/v1/tasks')
+      .expect(401)
+      .expect('Content-Type', /application\/json/);
+  });
+});
+
+describe('Tasks methods', () => {
+  var taskId;
+
+  test('should responde with a 201 status code', async () => {
+    const response = await api.post('/api/v1/auth').send({
+      username: 'agooduser002',
+      password: users[1].password,
+    });
+    await api
+      .post('/api/v1/tasks')
+      .set({
+        'x-access-token': response.body.token,
+      })
+      .send({
+        name: 'task1 test',
+        description: 'task1 description',
+        done: false,
+      })
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+      .expect((res) => {
+        expect(res.body.task).toBeDefined();
+      });
+  });
+
+  test('should responde with a 401 status code', async () => {
+    await api
+      .post('/api/v1/tasks')
+      .send({
+        name: 'task1',
+        description: 'task1 description',
+        done: false,
+      })
+      .expect(401)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('should responde with a 200 status code', async () => {
+    const response = await api.post('/api/v1/auth').send({
+      username: 'agooduser002',
+      password: users[1].password,
+    });
+    await api
+      .get('/api/v1/tasks')
+      .set({
+        'x-access-token': response.body.token,
+      })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect((res) => {
+        expect(res.body.tasks).toBeDefined();
+      });
+  });
+
+  test('should responde with a 200 status code', async () => {
+    const response = await api.post('/api/v1/auth').send({
+      username: 'agooduser002',
+      password: users[1].password,
+    });
+    await api
+      .get('/api/v1/tasks?limit=1')
+      .set({
+        'x-access-token': response.body.token,
+      })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect((res) => {
+        expect(res.body.tasks.length).toBe(1);
+        taskId = res.body.tasks[0]._id;
+      });
+  });
+
+  test('should responde with a 200 status code', async () => {
+    const response = await api.post('/api/v1/auth').send({
+      username: 'agooduser002',
+      password: users[1].password,
+    });
+
+    await api
+      .patch(`/api/v1/tasks/${taskId}`)
+      .set({
+        'x-access-token': response.body.token,
+      })
+      .send({
+        name: 'task1 test updated',
+        description: 'task1 description updated',
+        done: true,
+      })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect((res) => {
+        expect(res.body.task).toBeDefined();
+      });
+  });
+
+  test('should responde with a 200 status code', async () => {
+    const response = await api.post('/api/v1/auth').send({
+      username: 'agooduser002',
+      password: users[1].password,
+    });
+
+    await api
+      .delete(`/api/v1/tasks/${taskId}`)
+      .set({
+        'x-access-token': response.body.token,
+      })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect((res) => {
+        expect(res.body.task).toBeDefined();
+      });
   });
 });
 
